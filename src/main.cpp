cc86680ad5ac9d9ad44b540b30ad2cddfe3a33ec
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-07-18 02:18:58 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+struct Mouse
+{
+    float x, y;
+};
+
+float map(float value, float start1, float stop1, float start2, float stop2)
+{
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
+
 int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
@@ -14,21 +24,27 @@ int main(void)
 
     SDL_SetRenderVSync(renderer, true);
 
+    Mouse mouse;
+
     bool running = false;
     while (!running) {
         SDL_Event event;
-        SDL_PollEvent(&event);
-
-        switch (event.type) {
-        case SDL_EVENT_QUIT:
-            running = true;
-            break;
-        case SDL_EVENT_KEY_DOWN:
-            if (event.key.key == SDLK_Q)
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_EVENT_QUIT:
                 running = true;
+                break;
+            case SDL_EVENT_KEY_DOWN:
+                if (event.key.key == SDLK_Q)
+                    running = true;
+                break;
+            }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_GetMouseState(&mouse.x, &mouse.y);
+        Uint8 brightness = map(mouse.x, 0, 720, 0, 255);
+
+        SDL_SetRenderDrawColor(renderer, brightness, brightness, brightness, 0xff);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
     }
